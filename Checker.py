53a9e440bae0dc1ff65f67581f90319cf4e19196
OyuNet <oyunarda662@gmail.com>
2024-05-09 03:34:11 +0300

    Bug fixed add main.py.

--- Checker.py
+++ Checker.py
@@ -4,20 +4,20 @@ def isLinksFileEmpty():
     file = open("links.txt", "r")
     fileData = file.readlines()
 
-    if (len(fileData) < 1):
-        return True
-    else:
+    if (len(fileData) > 0):
         return False
-    
+    else:
+        return True
+
 def isFolderExists():
     isExists = os.path.exists("AppStorage")
-    
+
     if (not isExists):
         os.mkdir("AppStorage")
-        
+
 def isLinksFileExists():
     isExists = os.path.exists("links.txt")
-    
+
     if (not isExists):
         print("Your links.txt file is not exists. New one created.")
         file = open("links.txt", "x")
--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from Checker import isLinksFileEmpty, isFolderExists, isLinksFileExists
 
 def main():
     isLinksFileExists()
-    
-    if (isLinksFileEmpty):
+
+    if (isLinksFileEmpty()):
         print("Your links.txt file is empty. Fill it with your desired log lines to proceed.")
     else:
         fileData = open("links.txt", "r").readlines()
         isFolderExists()
-        
+
         for x in fileData:
             data = Extract(x)
             WriteData(data)
-            
+
         # Little Note to myself:
         # Maybe I could update links file in every for iteration, but I guess this thing uses much more compute power.
         # So I am going to skip that implementation. Just RemoveLines would be enough.
-        
+
         RemoveLines()
-    
+
 main()
